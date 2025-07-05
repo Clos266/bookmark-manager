5a30f0c0777b0a196c31c28980e252596e963a6d
Clos266 <clos266@gmail.com>
2025-07-05 13:30:42 +0200

    hero-first container responsive with image

--- src/components/atoms/Hero-img.jsx
+++ src/components/atoms/Hero-img.jsx
@@ -1,7 +1,7 @@
 import hero from "../../assets/images/img-hero.svg";
 
 function HeroImg() {
-  return <img src={hero} alt="Hero" className="img2" />;
+  return <img src={hero} alt="Hero" className="hero-img" />;
 }
 
 export default HeroImg;
--- src/components/organisms/Hero.jsx
+++ src/components/organisms/Hero.jsx
@@ -3,14 +3,20 @@ import HeroImg from "../atoms/Hero-img";
 
 function Hero() {
   return (
-    <header className="App-hero">
-       <HeroImg />
-      <h1>A Simple Bookmark Manager</h1>
-      <p>A clean and simple interface to organize your favourite websites. Open a new browser tab and see your sites load instantly. Try it for free.</p>
-      <Button>Get it on Chrome</Button>
-      <Button>Get it on Firefox</Button>
-     
-
+    <header className="hero">
+      <div className="hero-container">
+        <div className="hero-image">
+          <HeroImg />
+        </div>
+        <div className="hero-content">
+          <h1>A Simple Bookmark Manager</h1>
+          <p>A clean and simple interface to organize your favourite websites. Open a new browser tab and see your sites load instantly. Try it for free.</p>
+          <div className="hero-buttons">
+            <Button>Get it on Chrome</Button>
+            <Button>Get it on Firefox</Button>
+          </div>
+        </div>
+      </div>
     </header>
   );
 }
