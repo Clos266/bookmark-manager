e7674b04de4569a78638011f81f837014aaeaa9a
Clos266 <clos266@gmail.com>
2025-07-08 12:55:52 +0200

    add social icons and logo to harburger

--- src/components/molecules/Hamburger.jsx
+++ src/components/molecules/Hamburger.jsx
@@ -54,6 +54,7 @@ const HamburgerMenu = () => {
           <li><a href="/contact" onClick={toggleMenu}>CONTACT</a></li>
           <li><a href="/login" onClick={toggleMenu}><Button>LOGIN</Button></a></li>
         </ul>
+        {isOpen && (
         <div className="social-icons">
           <div className="social-burger">
           <svg className="icon" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
@@ -63,7 +64,7 @@ const HamburgerMenu = () => {
             <path d="M13.795 10.533 20.68 2h-3.073l-5.255 6.517L7.69 2H1l7.806 10.91L1.47 22h3.074l5.705-7.07L15.31 22H22l-8.205-11.467Zm-2.38 2.95L9.97 11.464 4.36 3.627h2.31l4.528 6.317 1.443 2.02 6.018 8.409h-2.31l-4.934-6.89Z"/>
           </svg>
           </div>
-        </div>
+        </div>)}
       </nav>
 
       {/* Overlay para cerrar el menú al hacer clic fuera */}
