9264843c8a78d94cd8c617ed0d45a8aaf870e6c5
Clos266 <clos266@gmail.com>
2025-07-22 22:35:04 +0200

    style: enhance hero section layout and button styles

--- src/components/organisms/Hero.jsx
+++ src/components/organisms/Hero.jsx
@@ -1,5 +1,4 @@
 import Button from "../atoms/ButtonWeb";
-
 import { images } from "../../assets/images";
 import StyledImage from "../atoms/ImagesStyled";
 
@@ -7,16 +6,20 @@ function Hero() {
   return (
     <header className="hero">
       <div className="hero-container">
-        <div className="hero-image">
-           <StyledImage src={images.hero} alt="hero image" />
-        </div>
         <div className="hero-content">
           <h1>A Simple Bookmark Manager</h1>
-          <p>A clean and simple interface to organize your favourite websites. Open a new browser tab and see your sites load instantly. Try it for free.</p>
+          <p>
+            A clean and simple interface to organize your favourite websites.
+            Open a new browser tab and see your sites load instantly. Try it for
+            free.
+          </p>
           <div className="hero-buttons">
             <Button>Get it on Chrome</Button>
-            <Button>Get it on Firefox</Button>
+            <Button className="inverted">Get it on Firefox</Button>
           </div>
+        </div>
+        <div className="hero-image">
+          <StyledImage src={images.hero} alt="hero image" />
         </div>
       </div>
     </header>
